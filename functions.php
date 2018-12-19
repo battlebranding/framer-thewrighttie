dcf631c627b5c595d26e76f7fe7770aea654d5ee
Marcus Battle <themarcusbattle@gmail.com>
2018-12-19 00:04:45 -0500

    Updated Google Font include

--- functions.php
+++ functions.php
@@ -3,8 +3,7 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'rokkitt-font', 'https://fonts.googleapis.com/css?family=Rokkitt' );
-    wp_enqueue_style( 'gochi-hand-font', 'https://fonts.googleapis.com/css?family=Gochi+Hand|Nanum+Pen+Script' );
+    wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Nanum+Pen+Script|Rokkitt' );
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'the-wright-tie', get_stylesheet_uri(), [ 'chameleon' ] );
 }
